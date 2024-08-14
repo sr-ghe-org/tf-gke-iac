86bcae6707b4aab7570caed6ce10b62812ac76a4
Syam Sankar <cgsyam@gmail.com>
2024-08-14 16:45:37 +0530

    Update gke_iowa.tf

--- root/gke_iowa.tf
+++ root/gke_iowa.tf
@@ -136,6 +136,7 @@ resource "time_sleep" "gke_iowa_wait_config_sync_install" {
 }
 
 /* Bind Workload Identity Federation for specific manifests (RootSync) to enable secure communication. */
+/*
 resource "google_service_account_iam_member" "gke_iowa_wif_binding" {
   for_each           = { for k, v in var.gke_resources.clusters.gke_iowa.manifests : k => v if v.manifest.kind == "RootSync" }
   service_account_id = "projects/${var.gke_configs.clusters.gke_iowa.project_id}/serviceAccounts/${google_service_account.gke_gar_sa.account_id}@${var.gke_configs.clusters.gke_iowa.project_id}.iam.gserviceaccount.com"
@@ -143,6 +144,8 @@ resource "google_service_account_iam_member" "gke_iowa_wif_binding" {
   member             = "serviceAccount:${var.gke_configs.clusters.gke_iowa.project_id}.svc.id.goog[${each.value.manifest.metadata.namespace}/root-reconciler-${each.value.manifest.metadata.name}]"
   depends_on         = [module.gke_iowa]
 }
+*/
+
 
 data "google_project" "gke_iowa_project" {
   project_id = var.gke_configs.clusters.gke_iowa.project_id
