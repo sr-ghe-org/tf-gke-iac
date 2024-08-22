d1f4127c45dce7ece686e965cfdb59e17b699609
SWETA_RAJ <swetaraj@google.com>
2024-08-22 14:38:31 +0530

    deleting the cluster

--- root/gke_outputs.tf
+++ root/gke_outputs.tf
@@ -1,18 +1,18 @@
-output "gke_iowa" {
-  value     = module.gke_iowa
-  sensitive = true
-}
+# output "gke_iowa" {
+#   value     = module.gke_iowa
+#   sensitive = true
+# }
 
-output "gke_toronto" {
-  value     = module.gke_toronto
-  sensitive = true
-}
+# output "gke_toronto" {
+#   value     = module.gke_toronto
+#   sensitive = true
+# }
 
-output "fleet_gke_iowa" {
-  value = module.fleet_gke_iowa
-}
+# output "fleet_gke_iowa" {
+#   value = module.fleet_gke_iowa
+# }
 
-output "fleet_gke_toronto" {
-  value = module.fleet_gke_toronto
-}
+# output "fleet_gke_toronto" {
+#   value = module.fleet_gke_toronto
+# }
 
