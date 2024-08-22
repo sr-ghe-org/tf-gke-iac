7cf22e51aa831c789415a726292e7167dcc21333
Sweta Raj <120810237+swetaraj-19@users.noreply.github.com>
2024-08-22 14:40:10 +0530

    deleting the cluster (#25)

--- root/gke_toronto.tf
+++ root/gke_toronto.tf
@@ -8,10 +8,10 @@
   5. Resource Deployment: Deploy the actual application components (runner sets, agent pools, etc.).
 */
 
-locals {
-  gke_toronto_controller_manifests     = { for k, v in var.gke_resources.clusters.gke_toronto.manifests : k => v if v.controller == true }
-  gke_toronto_non_controller_manifests = { for k, v in var.gke_resources.clusters.gke_toronto.manifests : k => v if v.controller == false }
-}
+# locals {
+#   gke_toronto_controller_manifests     = { for k, v in var.gke_resources.clusters.gke_toronto.manifests : k => v if v.controller == true }
+#   gke_toronto_non_controller_manifests = { for k, v in var.gke_resources.clusters.gke_toronto.manifests : k => v if v.controller == false }
+# }
 
 # resource "google_service_account" "gke_gar_sa" {
 #   project    = var.gke_configs.clusters.gke_toronto.project_id
@@ -25,103 +25,103 @@ locals {
 # }
 
 /* Bind Workload Identity User to the GAR service account */
-resource "google_service_account_iam_member" "gke_gar_sa_wif_binding_toronto" {
-  service_account_id = google_service_account.gke_gar_sa.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${var.gke_configs.clusters.gke_toronto.project_id}.svc.id.goog[config-management-system/root-reconciler]"
-  depends_on         = [module.gke_toronto]
-}
+# resource "google_service_account_iam_member" "gke_gar_sa_wif_binding_toronto" {
+#   service_account_id = google_service_account.gke_gar_sa.name
+#   role               = "roles/iam.workloadIdentityUser"
+#   member             = "serviceAccount:${var.gke_configs.clusters.gke_toronto.project_id}.svc.id.goog[config-management-system/root-reconciler]"
+#   depends_on         = [module.gke_toronto]
+# }
 
 /*
   --- CLUSTER CREATION ---
   Create the GKE cluster "gke_toronto" with specified configurations.
 */
-module "gke_toronto" {
-  source                     = "terraform-google-modules/kubernetes-engine/google//modules/beta-private-cluster"
-  version                    = "30.1.0"
-  project_id                 = var.gke_configs.clusters.gke_toronto.project_id
-  name                       = var.gke_configs.clusters.gke_toronto.name
-  region                     = var.gke_configs.clusters.gke_toronto.region
-  kubernetes_version         = var.gke_configs.clusters.gke_toronto.kubernetes_version
-  create_service_account     = var.gke_configs.clusters.gke_toronto.create_service_account
-  service_account_name       = var.gke_configs.clusters.gke_toronto.service_account_name
-  release_channel            = var.gke_configs.clusters.gke_toronto.release_channel
-  zones                      = var.gke_configs.clusters.gke_toronto.zones
-  network                    = var.gke_configs.clusters.gke_toronto.network
-  subnetwork                 = var.gke_configs.clusters.gke_toronto.subnetwork
-  network_project_id         = var.gke_configs.clusters.gke_toronto.network_project_id
-  ip_range_pods              = var.gke_configs.clusters.gke_toronto.ip_range_pods
-  ip_range_services          = var.gke_configs.clusters.gke_toronto.ip_range_services
-  http_load_balancing        = var.gke_configs.clusters.gke_toronto.http_load_balancing
-  network_policy             = var.gke_configs.clusters.gke_toronto.network_policy
-  horizontal_pod_autoscaling = var.gke_configs.clusters.gke_toronto.horizontal_pod_autoscaling
-  filestore_csi_driver       = var.gke_configs.clusters.gke_toronto.filestore_csi_driver
-  enable_private_endpoint    = var.gke_configs.clusters.gke_toronto.enable_private_endpoint
-  enable_private_nodes       = var.gke_configs.clusters.gke_toronto.enable_private_nodes
-  deletion_protection        = var.gke_configs.clusters.gke_toronto.deletion_protection
-  master_ipv4_cidr_block     = var.gke_configs.clusters.gke_toronto.master_ipv4_cidr_block
-  master_authorized_networks = var.gke_configs.clusters.gke_toronto.master_authorized_networks
-  node_pools                 = var.gke_configs.clusters.gke_toronto.node_pools
-  node_pools_oauth_scopes    = var.gke_configs.clusters.gke_toronto.node_pools_oauth_scopes
-  node_pools_labels          = var.gke_configs.clusters.gke_toronto.node_pools_labels
-  node_pools_metadata        = var.gke_configs.clusters.gke_toronto.node_pools_metadata
-  node_pools_taints          = var.gke_configs.clusters.gke_toronto.node_pools_taints
-  node_pools_tags            = var.gke_configs.clusters.gke_toronto.node_pools_tags
-  depends_on                 = [google_project_iam_member.sa_role_binding]
-}
+# module "gke_toronto" {
+#   source                     = "terraform-google-modules/kubernetes-engine/google//modules/beta-private-cluster"
+#   version                    = "30.1.0"
+#   project_id                 = var.gke_configs.clusters.gke_toronto.project_id
+#   name                       = var.gke_configs.clusters.gke_toronto.name
+#   region                     = var.gke_configs.clusters.gke_toronto.region
+#   kubernetes_version         = var.gke_configs.clusters.gke_toronto.kubernetes_version
+#   create_service_account     = var.gke_configs.clusters.gke_toronto.create_service_account
+#   service_account_name       = var.gke_configs.clusters.gke_toronto.service_account_name
+#   release_channel            = var.gke_configs.clusters.gke_toronto.release_channel
+#   zones                      = var.gke_configs.clusters.gke_toronto.zones
+#   network                    = var.gke_configs.clusters.gke_toronto.network
+#   subnetwork                 = var.gke_configs.clusters.gke_toronto.subnetwork
+#   network_project_id         = var.gke_configs.clusters.gke_toronto.network_project_id
+#   ip_range_pods              = var.gke_configs.clusters.gke_toronto.ip_range_pods
+#   ip_range_services          = var.gke_configs.clusters.gke_toronto.ip_range_services
+#   http_load_balancing        = var.gke_configs.clusters.gke_toronto.http_load_balancing
+#   network_policy             = var.gke_configs.clusters.gke_toronto.network_policy
+#   horizontal_pod_autoscaling = var.gke_configs.clusters.gke_toronto.horizontal_pod_autoscaling
+#   filestore_csi_driver       = var.gke_configs.clusters.gke_toronto.filestore_csi_driver
+#   enable_private_endpoint    = var.gke_configs.clusters.gke_toronto.enable_private_endpoint
+#   enable_private_nodes       = var.gke_configs.clusters.gke_toronto.enable_private_nodes
+#   deletion_protection        = var.gke_configs.clusters.gke_toronto.deletion_protection
+#   master_ipv4_cidr_block     = var.gke_configs.clusters.gke_toronto.master_ipv4_cidr_block
+#   master_authorized_networks = var.gke_configs.clusters.gke_toronto.master_authorized_networks
+#   node_pools                 = var.gke_configs.clusters.gke_toronto.node_pools
+#   node_pools_oauth_scopes    = var.gke_configs.clusters.gke_toronto.node_pools_oauth_scopes
+#   node_pools_labels          = var.gke_configs.clusters.gke_toronto.node_pools_labels
+#   node_pools_metadata        = var.gke_configs.clusters.gke_toronto.node_pools_metadata
+#   node_pools_taints          = var.gke_configs.clusters.gke_toronto.node_pools_taints
+#   node_pools_tags            = var.gke_configs.clusters.gke_toronto.node_pools_tags
+#   depends_on                 = [google_project_iam_member.sa_role_binding]
+# }
 
 /*
  --- FLEET REGISTRATION ---
  Register the GKE cluster with Google Cloud Fleet for centralized management.
 */
-module "fleet_gke_toronto" {
-  source                    = "terraform-google-modules/kubernetes-engine/google//modules/fleet-membership"
-  version                   = "30.1.0"
-  project_id                = var.gke_configs.clusters.gke_toronto.project_id
-  cluster_name              = module.gke_toronto.name
-  location                  = var.gke_configs.clusters.gke_toronto.region
-  membership_name           = var.gke_configs.clusters.gke_toronto.fleet_membership_name
-  enable_fleet_registration = var.gke_configs.clusters.gke_toronto.enable_fleet_registration
-  depends_on                = [module.gke_toronto]
-}
+# module "fleet_gke_toronto" {
+#   source                    = "terraform-google-modules/kubernetes-engine/google//modules/fleet-membership"
+#   version                   = "30.1.0"
+#   project_id                = var.gke_configs.clusters.gke_toronto.project_id
+#   cluster_name              = module.gke_toronto.name
+#   location                  = var.gke_configs.clusters.gke_toronto.region
+#   membership_name           = var.gke_configs.clusters.gke_toronto.fleet_membership_name
+#   enable_fleet_registration = var.gke_configs.clusters.gke_toronto.enable_fleet_registration
+#   depends_on                = [module.gke_toronto]
+# }
 
 /*
   --- CONFIG MANAGEMENT ---
   Enable configuration management features (Config Sync, Policy Controller) for the cluster.
 */
 
-resource "google_gke_hub_feature_membership" "gke_toronto_hub_feature_membership" {
-  provider   = google-beta
-  project    = var.gke_configs.clusters.gke_toronto.project_id
-  depends_on = [google_gke_hub_feature.gke_iowa_fleet_acm, google_service_account_iam_member.gke_gar_sa_wif_binding]
-  location   = "global"
-  feature    = "configmanagement"
-  membership = module.fleet_gke_toronto.cluster_membership_id
-  configmanagement {
-    version = var.gke_configs.clusters.gke_toronto.config_mgmt_version
+# resource "google_gke_hub_feature_membership" "gke_toronto_hub_feature_membership" {
+#   provider   = google-beta
+#   project    = var.gke_configs.clusters.gke_toronto.project_id
+#   depends_on = [google_gke_hub_feature.gke_iowa_fleet_acm, google_service_account_iam_member.gke_gar_sa_wif_binding]
+#   location   = "global"
+#   feature    = "configmanagement"
+#   membership = module.fleet_gke_toronto.cluster_membership_id
+#   configmanagement {
+#     version = var.gke_configs.clusters.gke_toronto.config_mgmt_version
 
-    dynamic "config_sync" {
-      for_each = var.gke_configs.clusters.gke_toronto.enable_config_sync ? [{ enabled = true }] : []
-      content {
-        source_format = "unstructured"
-        oci {
-          sync_repo                 = var.gke_configs.clusters.gke_toronto.config_sync_install_repo
-          secret_type               = "gcpserviceaccount"
-          gcp_service_account_email = google_service_account.gke_gar_sa.email
-        }
-      }
-    }
+#     dynamic "config_sync" {
+#       for_each = var.gke_configs.clusters.gke_toronto.enable_config_sync ? [{ enabled = true }] : []
+#       content {
+#         source_format = "unstructured"
+#         oci {
+#           sync_repo                 = var.gke_configs.clusters.gke_toronto.config_sync_install_repo
+#           secret_type               = "gcpserviceaccount"
+#           gcp_service_account_email = google_service_account.gke_gar_sa.email
+#         }
+#       }
+#     }
 
-    dynamic "policy_controller" {
-      for_each = var.gke_configs.clusters.gke_toronto.enable_policy_controller ? [{ enabled = true }] : []
-      content {
-        enabled                    = true
-        template_library_installed = true
-        referential_rules_enabled  = true
-      }
-    }
-  }
-}
+#     dynamic "policy_controller" {
+#       for_each = var.gke_configs.clusters.gke_toronto.enable_policy_controller ? [{ enabled = true }] : []
+#       content {
+#         enabled                    = true
+#         template_library_installed = true
+#         referential_rules_enabled  = true
+#       }
+#     }
+#   }
+# }
 
 /*
   --- PAUSE FOR CONFIG SYNC ---
@@ -134,20 +134,20 @@ resource "google_gke_hub_feature_membership" "gke_toronto_hub_feature_membership
 # }
 
 /* GAR Reader for GKE Node SA. */
-resource "google_project_iam_member" "gke_toronto_artifactregistry_reader" {
-  project = var.gke_configs.clusters.gke_toronto.project_id
-  role    = "roles/artifactregistry.reader"
-  member  = "serviceAccount:${module.gke_toronto.service_account}"
-}
+# resource "google_project_iam_member" "gke_toronto_artifactregistry_reader" {
+#   project = var.gke_configs.clusters.gke_toronto.project_id
+#   role    = "roles/artifactregistry.reader"
+#   member  = "serviceAccount:${module.gke_toronto.service_account}"
+# }
 
 /* Bind Workload Identity Federation for specific manifests (RootSync) to enable secure communication. */
-resource "google_service_account_iam_member" "gke_toronto_wif_binding" {
-  for_each           = { for k, v in var.gke_resources.clusters.gke_toronto.manifests : k => v if v.manifest.kind == "RootSync" }
-  service_account_id = google_service_account.gke_gar_sa.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${var.gke_configs.clusters.gke_toronto.project_id}.svc.id.goog[${each.value.manifest.metadata.namespace}/root-reconciler-${each.value.manifest.metadata.name}]"
-  depends_on         = [module.gke_toronto]
-}
+# resource "google_service_account_iam_member" "gke_toronto_wif_binding" {
+#   for_each           = { for k, v in var.gke_resources.clusters.gke_toronto.manifests : k => v if v.manifest.kind == "RootSync" }
+#   service_account_id = google_service_account.gke_gar_sa.name
+#   role               = "roles/iam.workloadIdentityUser"
+#   member             = "serviceAccount:${var.gke_configs.clusters.gke_toronto.project_id}.svc.id.goog[${each.value.manifest.metadata.namespace}/root-reconciler-${each.value.manifest.metadata.name}]"
+#   depends_on         = [module.gke_toronto]
+# }
 
 # data "google_project" "gke_toronto_project" {
 #   project_id = var.gke_configs.clusters.gke_toronto.project_id
